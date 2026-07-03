182b2b24cb5d6584c90658d322cfb40fb19af7c9
Vignesh Manoharan <vickmano91@gmail.com>
2026-07-03 11:48:04 +0000

    Add lesson 03 Variables practice

---
+++ practice/lesson_03_variables.py
@@ -0,0 +1,32 @@
+# Lesson 03: Variables
+
+name = "Vignesh"
+age = 34
+is_learning_python = True
+current_role = "Senior Tableau Developer"
+
+print(name)
+print(age)
+print(is_learning_python)
+print(current_role)
+
+print("My name is", name)
+print("My age is", age)
+print("Am I learning Python", is_learning_python)
+print("My current role is", current_role)
+
+monthly_salary = 252000
+years_of_experience = 11
+favorite_tool = "Tableau"
+learning_goal = "Use python for automation and data work"
+
+print("Monthly Salary:", monthly_salary)
+print("Years of experience", years_of_experience)
+print("Favorite tool", favorite_tool)
+print("Learning Goal", learning_goal)
+
+age = 35
+favorite_tool = "Python"
+
+print("updated age:", age)
+print("updated favorite tool", favorite_tool)
