e3d92c05f0d70f3911b90a144498413fee362587
Vignesh Manoharan <vickmano91@gmail.com>
2026-07-03 16:55:11 +0000

    Add lesson 04 numbers practice

---
+++ practice/lesson_04_numbers.py
@@ -0,0 +1,45 @@
+# Lesson 04: Numbers
+
+age = 34
+monthly_salary = 252000
+height_cm = 189.5
+home_loan_interest_rate = 7.35
+is_learning_python = True
+has_completed_python = False
+
+print("Age:", age)
+print("Monthly Salary:", monthly_salary)
+print("Height in cm:", height_cm)
+print("Home Loan interest rate:", home_loan_interest_rate)
+print("Is learning Python:", is_learning_python)
+print("Has completed Python:", has_completed_python)
+
+print("Type of Age:", type(age))
+print("Type of monthly salary:", type(monthly_salary))
+print("Type of Height in cm:", type(height_cm))
+print("Type of Home Loan interest rate:", type(home_loan_interest_rate))
+print("Type of Is learning Python:", type(is_learning_python))
+
+annual_salary = monthly_salary * 12
+print("Annual Salary", annual_salary)
+
+next_year_age = age + 1
+print("Age next year:", next_year_age)
+
+half_salary = monthly_salary / 2
+print("Half Salary:", half_salary)
+
+emi = 50347
+personal_loan_emi = 22350
+
+total_monthly_emi = emi + personal_loan_emi
+remaining_salary = monthly_salary - total_monthly_emi
+
+print("Total monthly EMI:", total_monthly_emi)
+print("Remaining Salary after emi:", remaining_salary)
+
+print("True + True:", True + True)
+print("10 / 3:", 10 / 3)
+print("10 // 3:", 10 // 3)
+print("10 % 3:", 10 % 3)
+print("5 ** 2:", 5**2)
