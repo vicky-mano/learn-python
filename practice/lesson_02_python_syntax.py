68bd12458986b36e074fa69d051a51c33ca1c68f
Vignesh Manoharan <vickmano91@gmail.com>
2026-07-03 11:08:45 +0000

    Add lesson 02 Python syntax practice

---
+++ practice/lesson_02_python_syntax.py
@@ -0,0 +1,14 @@
+# Lesson 02: Python Syntax
+print("Python reads code from top to bottom")
+print("Each Instruction is usually written on seperate line")
+
+# Indentation means space at beginning of a line.
+# Python uses indentation to understand code blocks.
+
+is_learning = True
+
+if is_learning:
+    print("Vignesh is learning python")
+    print("This line belongs inside the if block")
+
+print("This line is outside if block")
