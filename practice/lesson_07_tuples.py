41273d8e9f59a7ac471786cfa2f374417577aea2
Vignesh Manoharan <vickmano91@gmail.com>
2026-08-29 09:24:21 +0000

    Lesson 7: Practice continue

---
+++ practice/lesson_07_tuples.py
@@ -0,0 +1,27 @@
+# Lesson 7: Tuples
+
+print("Lesson 7A: Tuples Basic")
+
+fruits_tuple = ("apple", "banana", "cherry")
+
+print("Fruits Tuple: ", fruits_tuple)
+
+print("First Fruit: ", fruits_tuple[0])
+print("Second Fruit: ", fruits_tuple[1])
+print("Third Fruit: ", fruits_tuple[2])
+
+print("Tuple type: ", type(fruits_tuple))
+
+print("Tuple Length: ", len(fruits_tuple))
+
+colors = ("red", "green", "blue")
+
+print("Full colors tuple: ", colors)
+
+print("First color: ", colors[0])
+print("Second color: ", colors[1])
+print("Third color: ", colors[2])
+
+print("Length of colors tuple: ", len(colors))
+
+print("Type of colors tuple: ", type(colors))
